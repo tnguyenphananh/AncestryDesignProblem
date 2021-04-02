2d28ab6ab7aa27e026620ccb2d2f0c123325e757
tnguyenphananh <tnguyenphananh@knox.edu>
2021-04-01 22:16:58 -0500

    fix bug

--- src/dna/DnaSequence.java
+++ src/dna/DnaSequence.java
@@ -14,15 +14,15 @@ public class DnaSequence {
         // TODO: fix me
         List<String> aminoAcids = new LinkedList<>();
         int i = 0;
-        while(i < dna.length()) {
-            String triplet = "" + dna.charAt(i) + dna.charAt(i+1) + dna.charAt(i+2);
+        while (i < dna.length()) {
+            String triplet = "" + dna.charAt(i) + dna.charAt(i + 1) + dna.charAt(i + 2);
             try {
                 String acid = this.dnaCodon.acidFor(triplet);
                 aminoAcids.add(acid);
             } catch (Exception e) {
                 // silently pass codon that does not transcribe a amino acid
             }
-            i += 1;
+            i += 3;
         }
         return aminoAcids;
     }
